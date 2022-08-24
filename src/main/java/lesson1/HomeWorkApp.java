d5a54668a0908b1a94ca39cf69bd2e7187eeb9d7
ibrokhimal <102519725+ibrokhimal@users.noreply.github.com>
2022-08-24 15:36:32 +0500

    well done lesson one homeWorkApp!

--- src/main/java/lesson1/HomeWorkApp.java
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -1,5 +1,6 @@
-public class HomeWorkApp {
+package lesson1;
 
+public class HomeWorkApp {
     public static void main(String[] args) {
         checkSumSing(1,-1);
         printThreeWords();
@@ -11,28 +12,38 @@ public class HomeWorkApp {
         int a = i;
         int b = i1;
 
-        if (a >= b) System.out.println("a >= b");
-        else System.out.println("a > b");
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a > b");
+        }
     }
 
     private static void printColor(int i) {
-        if (i <= 0)
+        if (i <= 0) {
             System.out.println("Red");
-        else if (i <= 100)
+        }
+        else if (i <= 100) {
             System.out.println("Yellow");
-        else
+        }
+        else {
             System.out.println("Green");
+        }
     }
 
-    private static void checkSumSing(int a, int b) {
-        if ((a + b) >= 0)
-            System.out.println("Summa polojitelno");
-        else
-            System.out.println("Summa otritsatelno");
-    }
     private static void printThreeWords(){
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+    private static void checkSumSing(int a, int b) {
+        if ((a + b) >= 0) {
+            System.out.println("Summa polojitelno");
+        }
+        else {
+            System.out.println("Summa otritsatelno");
+        }
+    }
 }
+
